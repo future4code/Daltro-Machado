a2a57c629848008eaade5e24385766e114d23fff
daltrowilliam <daltrowilliam@gmail.com>
2020-09-30 16:44:23 -0300

    Exercicios de Condicionais JS completos com Desafios

---
+++ semana3/operadores/index.js
@@ -0,0 +1,121 @@
+/* Interpretação de Código
+
+Exercício 1
+
+a.  false
+b.  false
+c.  true
+e.  boolean
+
+
+Exercício 2
+
+
+a.  undefined
+b.  null
+c.  11
+d.  3
+e.  [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+f.  9
+
+
+Escrita de Código
+
+Exercício 1*/
+
+// let idadeUsuario = Number(prompt("Qual a sua idade?"))
+// let idadeAmigo = Number(prompt("Qual a idade do seu melhor amigo?"))
+// console.log("Sua idade é maior do que a do seu melhor amigo?", idadeUsuario > idadeAmigo)
+// console.log("Diferença entre as idades:", idadeUsuario - idadeAmigo)
+
+
+/* Exercício 2 Letras a, b */
+
+// let numeroPar = Number(prompt("Por favor, digite um número par"))
+// console.log("Resto da divisão por 2:", numeroPar%2)
+
+/* Letras c, d
+
+c. Para qualquer número par digitado o resto da divisão será igual a 0.
+
+d. Quando um número impar for digitado o resto da divisão será sempre diferente de 0.
+*/
+
+/* Exercício 3 */
+
+// let listaDeTarefas = []
+// listaDeTarefas.push(prompt("Digite sua primeira tarefa do dia."))
+// listaDeTarefas.push(prompt("Digite sua segunda tarefa do dia."))
+// listaDeTarefas.push(prompt("Digite sua terceira tarefa do dia."))
+// console.log(listaDeTarefas)
+// /* Preferi deixar o usuario digitar os valores normais da tarefa, 1,2 ou 3, e subtraí 1 para ajustar ao ínidice do array*/
+// let tarefaRealizada = (Number(prompt("Digite o número da tarefa já realizada.")) - 1)
+// listaDeTarefas.splice(tarefaRealizada, 1)
+// console.log(listaDeTarefas)
+
+
+/* Exercício 4 */
+
+// let nomeDoUsuario = prompt("Qual o seu nome?")
+// let emailDoUsuario = prompt("Qual o seu email?")
+// console.log("O e-mail", emailDoUsuario, "foi cadastrado com sucesso. Seja bem-vinda(o),", nomeDoUsuario + "!")
+
+
+/* Desafio 1 */
+
+/* let kelvin = (77-32)*5/9 + 273.15
+console.log(kelvin + "K")
+
+let fahr = (80)*9/5 + 32
+console.log(fahr + "ºF")
+
+fahr = (30)*9/5 + 32
+kelvin = (fahr-32)*5/9 + 273.15
+console.log(fahr + "ºF")
+console.log(kelvin + "K")
+
+let celsius = Number(prompt("Digite a temperatura em graus Celsius."))
+fahr = (celsius)*9/5 + 32
+kelvin = (fahr-32)*5/9 + 273.15
+console.log("Convertendo", celsius + "ºC")
+console.log(fahr + "ºF")
+console.log(kelvin + "K")
+ */
+
+
+/* Desafio 2 */
+
+/* let valorConsumo = 280 * 0.05
+console.log("O valor para um consumo de 280 quilowatts é R$", valorConsumo)
+let consumo = Number(prompt("Digite a quantidade de quilowatts consumida:"))
+valorConsumo = consumo * 0.05
+console.log("O valor para um consumo de", consumo, "quilowatts é R$", valorConsumo)
+let desconto = Number(prompt("Digite o percentual de desconto(%):"))
+valorConsumo = (consumo * 0.05)
+let valorDesconto = (valorConsumo) * ((100 - desconto)/100)
+console.log("O valor para um consumo de", consumo, "quilowatts, com o desconto de", desconto + "% é R$", valorDesconto)
+
+ */
+/* Desafio 3 */
+
+/* let kilos = 20 * 0.453592
+console.log("20lb equivalem a", kilos + "Kg")
+
+kilos = 10.5 * 0.0283495
+console.log("10.5oz equivalem a", kilos + "Kg")
+
+let metros = 100 * 1609.34
+console.log("100mi equivalem a", metros + "m")
+
+metros = 50 * 0.3048
+console.log("50ft equivalem a", metros + "m")
+
+let litros = 103.56 * 3.78541
+console.log("103.56gal equivalem a", litros + "l")
+
+litros = 450 * 0.284131
+console.log("450 xic equivalem a", litros + "l")
+
+let milhas = Number(prompt("Digite a quantidade de milhas que deseja converter para metros:"))
+metros = milhas * 1609.34
+console.log(milhas + "mi equivalem a", metros + "m") */
