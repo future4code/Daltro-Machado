08e434fc45965bc03e9f3e09f9b7ea2cb2e50e6b
daltrowilliam <daltrowilliam@gmail.com>
2020-10-09 12:03:34 -0300

    Interpretação de Código - Exercicio 2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -14,5 +14,20 @@ retonando como saída dafunção a string "R$" concatenada com o valor calculado
 // Execicio 2
 
 /*
+O código calcula e exibe o valor de um investimento de acordo com o investimento escolhido.
+Da forma como foi escrito, ele irá sempre armazenar nas variáveis (novoMontante) e (segundoMontante)
+respectivamente o valor de 150 investidos em Ações e 200 investidos no Tesouro Direto.
+A função "investeDinheiro()" irá, em momentos distintos, receber estes dois parâmetros (tipo de
+investimento e valor) calculando o valor após o investimento, multiplicando o valores recebidos (150 e 200)
+por um multiplicador pré-definido de acordo com o investimento informado (Ações e Tesouro Direto).
+A cada execução da função ela irá armazenar na variável (valorAposInvestimento) o resultado da multiplicação.
+O retorno da função será a variável (valorAposInvestimento).
+Existe uma teste de o tipo de investimento não ser válido, mas como os valores passados para afunção estão
+pré-definidos no código, a mensagem de alerta não será mostrada.
+*/
+
+// Exercício 3
+
+/*
 
 */
