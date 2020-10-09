6e7c3db1c6da0ea6586598d9d2b2bd4ab9294565
daltrowilliam <daltrowilliam@gmail.com>
2020-10-09 11:46:15 -0300

    Interpretação de Códico - Exercicio 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,18 @@
+// Lista de Exercícios
+
+// Interpretação de Código
+
+// Exercicio 1
+
+/* Este código está armazenando na variavel (meuDinheiro) o valor de 100 dolares processado pela
+função "conversorDeMoeda()". o valor de 100 é o parâmetro passado para a função, que irá solicitar
+ao usuário que digite o valor da cotação do dólar e após converter essa cotação para número e
+armazená-la na variável (cotacao), irá multiplicar esta variável (cotacao) pelo parâmetro recebido (100),
+retonando como saída dafunção a string "R$" concatenada com o valor calculado.
+*/
+
+// Execicio 2
+
+/*
+
+*/
