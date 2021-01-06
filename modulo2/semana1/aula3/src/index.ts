6f2e1d1f2d61b086ad3b4afc9d97f6570ac3d865
daltrowilliam <daltrowilliam@gmail.com>
2021-01-06 18:14:27 -0300

    Exercicios Finalizados

--- modulo2/semana1/aula3/src/index.ts
+++ modulo2/semana1/aula3/src/index.ts
@@ -7,10 +7,8 @@ const app: Express = express();
 app.use(express.json());
 app.use(cors());
 
-// a função express() inicia a aplicação web com express
-// os .use() ativam os módulos de Bodyparser e Cors
 
-// Criando o Enpoint 1 (GET ALL)
+// Criando o Endpoint 1 (GET ALL)
 
 app.get('/countries/all', (req: Request, res: Response) => {
     const result = countries.map(country => ({
@@ -24,7 +22,7 @@ app.get('/countries/all', (req: Request, res: Response) => {
     })
 
 
-// Criando o Enpoint 3 (GET SEARCH)
+// Criando o Endpoint 3 (GET SEARCH)
 
     app.get("/countries/search", (req: Request, res: Response) => {
 
@@ -58,7 +56,7 @@ app.get('/countries/all', (req: Request, res: Response) => {
   })
 
 
-// Criando o Enpoint 2 (GET ID)
+// Criando o Endpoint 2 (GET ID)
 
 app.get('/countries/:id', (req: Request, res: Response) => {
     const result: country | undefined = countries.find(
@@ -79,25 +77,44 @@ app.get('/countries/:id', (req: Request, res: Response) => {
       
       if (req.params.id) {
          const id: string = req.params.id;
-         if ((req.body.name) || (req.body.capital))  {
-            const name: string = req.body.name;
-            const capital: string = req.body.capital;
-            countries.forEach((country) => {
-               if (country.id === Number(id)) {
-                  country.name = name
-                  country.capital = capital
-               }
-            })
+
+         if ((req.body.name) || (req.body.capital)) {
+            
+            if (req.body.name)  {
+               const name: string = req.body.name;
+               countries.forEach((country) => {
+                  if (country.id === Number(id)) {
+                     country.name = name
+                  }
+                  else {
+                     res.status(404).send("Id Not Found")  
+                  }
+               })
+            }
+
+            if (req.body.capital)  {
+               const capital: string = req.body.capital;
+               countries.forEach((country) => {
+                  if (country.id === Number(id)) {
+                     country.capital = capital
+                  }
+                  else {
+                     res.status(404).send("Id Not Found")  
+                  }
+               })
+            }
+            
+            res.status(200).send("Success!")
+
          } else {
             res.status(400).send("Invalid Parameters")
          }
-         
+                        
       } else {
          res.status(400).send("Invalid Parameters")
       }
    });
-
-
+   
  app.listen(3003, () => {
     console.log("Servidor rodando na porta 3003")
  })
