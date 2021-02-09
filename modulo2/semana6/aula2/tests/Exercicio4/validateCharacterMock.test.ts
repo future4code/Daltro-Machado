c18cc7f4cae085ab966cfe653b81fa50680ca9ac
daltrowilliam <daltrowilliam@gmail.com>
2021-02-09 16:28:53 -0300

    Exercicios 1 a 5 Prontos. Exercio 6 feito parcialmente

---
+++ modulo2/semana6/aula2/tests/Exercicio4/validateCharacterMock.test.ts
@@ -0,0 +1,15 @@
+// Letra b
+
+test("Creating Mocks", () => {
+    const validatorMock = jest.fn(() => {
+			return true
+		})
+})
+
+// Letra c
+
+test("Creating Mocks", () => {
+    const validatorMock = jest.fn(() => {
+			return false
+		})
+})
